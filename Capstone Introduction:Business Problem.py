39be2bda430cd30fb115ec5783ad551439818320
nb970 <nblush970@gmail.com>
2020-07-06 20:23:39 -0700

    Add files via upload

---
+++ Capstone Introduction:Business Problem.py
@@ -0,0 +1,7 @@
+/* 
+1. Introduction
+	During the 2nd season of the CBS crime drama, Elementary, two characters speak about the character of New York City where the show takes place. The two characters are both from London and one makes the remark that New York City is the London of the United States. This conversation got me to wonder how alike are the 2 cities or how dissimilar are they to each other. With both of the cities being major economic hubs and each of their countries most densely populated cities one could posit these two cities could be very similar to one another.
+
+2. Business Problem
+	Let’s assume a major corporation or business is looking to go international and they would like to move to a metropolitan area in which the city’s layout is similar to one where the company currently resides in order to achieve the same amount of business or interact with similar clientele. Now let’s assume the company could be in either New York City or London and the company wants to move to the other mentioned city. Would the two cities be similar enough so the company would be able to sustain the same amount of business without repercussion? This analysis hopes to answer that question.
+*/
